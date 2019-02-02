bafde477fe5e25dc1753383997929d3a0f2c5b58
shin <proscus0706@gmail.com>
2019-02-02 20:19:28 +0900

    My First VR Project

---
+++ Assets/Scirpts/MaterialGradientModifier.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (Renderer))] //안정성을 높여준다. This class requires Renderer. 이러한 것들을 Attribute라고 한다.
+public class MaterialGradientModifier : MonoBehaviour
+{
+    Renderer _renderer; //first way to get Component.
+    [SerializeField] Gradient gradient;
+
+    float _gradientPosition;
+    public float gradientPosition
+    {
+        get { return _gradientPosition; }
+        set
+        {
+            if(_gradientPosition != value)
+            {
+                _gradientPosition = value;
+                _renderer.material.color = gradient.Evaluate(_gradientPosition);
+            }
+        }
+    }
+
+    //Change the Value here
+    //void SetGradientPosition(float position)
+    //{
+        //if (position == gradientPosition) 
+            //position과 gradientPosition 값이 같으면 gradient가 아니기 때문에 막아놓음.
+            //return;
+
+        //gradientPosition = position;
+        //_renderer.material.color = gradient.Evaluate(gradientPosition);
+        //first way to get Component. --- Evaluate를 하면 이미 gradient 객체가 형성된것이다. 파라미터는 초기 설정값인듯 하다.
+    //}
+
+    void Awake() //this method is called before unity starts 
+    {
+        _renderer = GetComponent<Renderer>(); 
+        //Second Way to get the Component 
+        //It's going to take an Object from the same object where the script exists.
+    }
+
+    // Start is called before the first frame update
+    void Start() //It's a message sending to monoBehaviour 
+    {
+        gradientPosition = 0;
+        //SetGradientPosition(0);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        //gradientPosition = Mathf.Sin(((Time.time)) * 0.5f) + 0.5f;
+        //SetGradientPosition((Mathf.Sin(Time.time) * 0.5f) + 0.5f);
+    //}
+
+
+}
---
+++ Assets/Scirpts/ObjectFocus.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocus : MonoBehaviour
+{
+    [SerializeField] Transform reference; //카메라 위치를 참조한다.
+
+    [SerializeField] float minAngle = 10;
+    [SerializeField] float maxAngle = 30;
+    
+
+    private float _fadeAmount = -1;
+    public float fadeAmount
+    {
+        get { return _fadeAmount; }
+        set
+        {
+            if(value != fadeAmount)
+            {
+                _fadeAmount = value;
+            }
+        }
+    }
+
+    private float _delta = -1; //_fadeAmount와 값을 비교하기 위해 만든 변수.   measure the angle and the distance.
+    public float delta
+    {
+        get { return _delta; }
+        set
+        {
+            if(value != _delta)
+            {
+                _delta = value;
+                // TODO : Update Fade Amount Value.
+                //FadeAmount.
+                fadeAmount = Mathf.Lerp(maxAngle, minAngle, _delta);  //Lerp - interpolate - 중간값을 채움.
+            }
+        }
+    }
+
+    void Fade()
+    {
+        // TODO : Update Delta
+        //Debug.DrawLine(reference.position, transform.position, Color.yellow);   //여기서 reference는 카메라인거 알지? ㅎ
+    
+        Vector3 distance = (transform.position - reference.position).normalized; //현재위치와 카메라위치를 빼줌. 즉 거리를 구함, //Normalized -- x,y,z position 값을 일정하게 만든다.
+        Debug.DrawRay(reference.position, reference.forward, Color.cyan);  //forwoard direction. what camera's looking into.
+        Debug.DrawRay(reference.position, distance, Color.yellow);
+        delta = Vector3.Angle(distance, reference.forward);
+    }
+
+   
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //reference = GameObject.FindGameObjectWithTag("MainCamera").transform;                  --this is the one way
+        if (reference == null) { 
+            reference = Camera.main.transform;  //Another way to reference camera position.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fade();
+    }
+
+    //Useful way to program graphically 
+    void OnGUI()
+    {
+        GUILayout.Label("delta : " + delta.ToString());
+        GUILayout.Label("fadeAmount : " + fadeAmount.ToString());
+    }
+}
